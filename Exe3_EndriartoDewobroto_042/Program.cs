93f49d47f3ac4be7ecf9234b08ee8e9dd202501b
E Dewobroto <e.dewobroto.ft21@mail.umy.ac.id>
2022-11-23 15:32:05 +0700

    membuat class node, mendeklarasikan variabel yang ada dan membuat node next untuk circularlist

--- Exe3_EndriartoDewobroto_042/Program.cs
+++ Exe3_EndriartoDewobroto_042/Program.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace Exe3_EndriartoDewobroto_042
 {
+    class Node
+    {
+        //create nodes for the circular nexted list
+        public int StudentNumber;
+        public string StudentName;
+        public Node next;
+    }
     internal class Program
     {
         static void Main(string[] args)
